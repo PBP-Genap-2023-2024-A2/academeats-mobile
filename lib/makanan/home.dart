263a68e34740fda01d623061244371d4e677947a
nairisfa <nailakrisnazulfa@gmail.com>
2024-05-24 21:12:54 +0700

    updating makanan

---
+++ lib/makanan/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'edit.dart';
+
+class MakananHome extends StatelessWidget {
+  const MakananHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Makanan Home'),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Add navigation or action here
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Add navigation or action here
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text('Welcome to Makanan Home!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => EditMakananPage()),
+          );
+        },
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
