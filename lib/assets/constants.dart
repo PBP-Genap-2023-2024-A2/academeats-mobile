278d4092b84bfdf9bc2f7546831381e85d3a100d
crlnravel <carleanoravel@gmail.com>
2024-05-26 19:37:32 +0700

    Add base model

---
+++ lib/assets/constants.dart
@@ -0,0 +1,4 @@
+import 'dart:core';
+
+const String BACKEND_URI_LOCAL = "http://localhost:8000/";
+const String BACKEND_URI = "http://carleano-ravelza-academeats.pbp.cs.ui.ac.id/";
--- lib/utils/fetch.dart
+++ lib/utils/fetch.dart
@@ -1,1 +1,61 @@
-import 'package:http'
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:academeats_mobile/assets/constants.dart' as Constant;
+
+enum RequestMethod {
+  get,
+  post,
+  patch,
+  put,
+  delete
+}
+
+extension RequestMethodExtension on RequestMethod {
+  String get value {
+    switch (this) {
+      case RequestMethod.get:
+        return "GET";
+      case RequestMethod.post:
+        return "POST";
+      case RequestMethod.patch:
+        return "PATCH";
+      case RequestMethod.put:
+        return "PUT";
+      case RequestMethod.delete:
+        return "DELETE";
+    }
+  }
+}
+
+Future<Map<String, dynamic>> fetchData(String path,
+    {
+      RequestMethod method = RequestMethod.get,
+      Map<String, dynamic>? body,
+    }) async {
+
+  var uri = Uri(
+    scheme: 'http',
+    host: Constant.BACKEND_URI_LOCAL,
+    path: path,
+  );
+
+  // Setting the request object
+  var request = http.Request(method.value, uri);
+  request.headers.addAll({
+    "Access-Control-Allow_Origin": "*",
+    "Content-Type": "application/json",
+  });
+
+  // Send the request to the server
+  var response = await http.Client().send(request);
+
+  String responseBody = '';
+
+  // Transform the response body to string using utf8 format
+  await response.stream.transform(utf8.decoder).forEach((element) {
+    responseBody += element;
+  });
+
+  return jsonDecode(responseBody);
+}
